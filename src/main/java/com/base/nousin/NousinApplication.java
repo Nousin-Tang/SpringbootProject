ccd6ca590db64ed308573f6915b9d679bf6da942
unnous <unnous@163.com>
2019-11-30 16:15:33 +0800

    初始化项目

---
+++ src/main/java/com/base/nousin/NousinApplication.java
@@ -0,0 +1,13 @@
+package com.base.nousin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NousinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NousinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/base/nousin/NousinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.base.nousin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NousinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
